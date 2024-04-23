25782279b3746436dac9b33ff50f309f86692382
mazratul <97ratulmazumder@gmail.com>
2024-04-22 22:11:31 -0400

    initial commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,25 +1,13 @@
 const { MongoClient } = require("mongodb");
-const fs = require('fs').promises;
 
 const handler = async (event, context) => {
-  const uri = "mongodb+srv://mazumder:rmazu1mazumder@cluster0.dbbf0lb.mongodb.net/?retryWrites=true&w=majorit";
-  const client = new MongoClient(uri);
+  const uri =
+  "mongodb+srv://mazumder:rmazu1mazumder@cluster0.dbbf0lb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
   try {
     await client.connect();
     const data = await findPokemonData(client);
-
-    if (data.length === 0) { 
-      const errorPage = await fs.readFile('../../errorPages/404.html', 'utf8');  
-      return {
-        statusCode: 404,
-        headers: {
-          "Content-Type": "text/html"
-        },
-        body: errorPage
-      };
-    }
-
     return {
       statusCode: 200,
       headers: {
